f548f92f67cc9d87c4af9c054577ed28ea6aa4a6
Ali Chraghi <alichraghi@proton.me>
2023-07-26 02:08:52 +0330

    init

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
     _ = b.addModule("mach-flac", .{ .source_file = .{ .path = "src/lib.zig" } });
@@ -11,40 +12,71 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+
+    const cmake_config_header = b.addConfigHeader(
+        .{
+            .style = .{ .cmake = .{ .path = "flac/config.cmake.h.in" } },
+            .include_path = "config.h",
+        },
+        .{
+            .CPU_IS_BIG_ENDIAN = builtin.target.cpu.arch.endian() == .Big,
+            .FLAC__CPU_ARM64 = builtin.target.cpu.arch.isAARCH64(),
+            .ENABLE_64_BIT_WORDS = builtin.target.ptrBitWidth() == 64,
+            .FLAC__ALIGN_MALLOC_DATA = builtin.target.cpu.arch.isX86(),
+            .FLAC__SYS_DARWIN = builtin.target.isDarwin(),
+            .FLAC__SYS_LINUX = builtin.target.os.tag == .linux,
+            .HAVE_BYTESWAP_H = true,
+            .HAVE_CPUID_H = true,
+            .HAVE_FSEEKO = true,
+            .HAVE_INTTYPES_H = true,
+            .HAVE_MEMORY_H = true,
+            .HAVE_STDINT_H = true,
+            .HAVE_STDLIB_H = true,
+            .HAVE_STRING_H = true,
+            .HAVE_TYPEOF = true,
+            .HAVE_UNISTD_H = true,
+        },
+    );
+    main_test.addConfigHeader(cmake_config_header);
+    main_test.addIncludePath("flac/include");
+    main_test.addIncludePath("flac/src/libFLAC/include");
+    main_test.addCSourceFiles(&sources, &.{});
+    main_test.defineCMacro("HAVE_CONFIG_H", null);
     main_test.linkLibC();
+    b.installArtifact(main_test);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(main_test).step);
 }
 
 const sources = [_][]const u8{
-    "src/libFLAC/bitmath.c",
-    "src/libFLAC/bitreader.c",
-    "src/libFLAC/bitwriter.c",
-    "src/libFLAC/cpu.c",
-    "src/libFLAC/crc.c",
-    "src/libFLAC/fixed.c",
-    "src/libFLAC/fixed_intrin_sse2.c",
-    "src/libFLAC/fixed_intrin_ssse3.c",
-    "src/libFLAC/fixed_intrin_sse42.c",
-    "src/libFLAC/fixed_intrin_avx2.c",
-    "src/libFLAC/float.c",
-    "src/libFLAC/format.c",
-    "src/libFLAC/lpc.c",
-    "src/libFLAC/lpc_intrin_neon.c",
-    "src/libFLAC/lpc_intrin_sse2.c",
-    "src/libFLAC/lpc_intrin_sse41.c",
-    "src/libFLAC/lpc_intrin_avx2.c",
-    "src/libFLAC/lpc_intrin_fma.c",
-    "src/libFLAC/md5.c",
-    "src/libFLAC/memory.c",
-    "src/libFLAC/metadata_iterators.c",
-    "src/libFLAC/metadata_object.c",
-    "src/libFLAC/stream_decoder.c",
-    "src/libFLAC/stream_encoder.c",
-    "src/libFLAC/stream_encoder_intrin_sse2.c",
-    "src/libFLAC/stream_encoder_intrin_ssse3.c",
-    "src/libFLAC/stream_encoder_intrin_avx2.c",
-    "src/libFLAC/stream_encoder_framing.c",
-    "src/libFLAC/window.c",
+    "flac/src/libFLAC/bitmath.c",
+    "flac/src/libFLAC/bitreader.c",
+    "flac/src/libFLAC/bitwriter.c",
+    "flac/src/libFLAC/cpu.c",
+    "flac/src/libFLAC/crc.c",
+    "flac/src/libFLAC/fixed.c",
+    "flac/src/libFLAC/fixed_intrin_sse2.c",
+    "flac/src/libFLAC/fixed_intrin_ssse3.c",
+    "flac/src/libFLAC/fixed_intrin_sse42.c",
+    "flac/src/libFLAC/fixed_intrin_avx2.c",
+    "flac/src/libFLAC/float.c",
+    "flac/src/libFLAC/format.c",
+    "flac/src/libFLAC/lpc.c",
+    "flac/src/libFLAC/lpc_intrin_neon.c",
+    "flac/src/libFLAC/lpc_intrin_sse2.c",
+    "flac/src/libFLAC/lpc_intrin_sse41.c",
+    "flac/src/libFLAC/lpc_intrin_avx2.c",
+    "flac/src/libFLAC/lpc_intrin_fma.c",
+    "flac/src/libFLAC/md5.c",
+    "flac/src/libFLAC/memory.c",
+    "flac/src/libFLAC/metadata_iterators.c",
+    "flac/src/libFLAC/metadata_object.c",
+    "flac/src/libFLAC/stream_decoder.c",
+    "flac/src/libFLAC/stream_encoder.c",
+    "flac/src/libFLAC/stream_encoder_intrin_sse2.c",
+    "flac/src/libFLAC/stream_encoder_intrin_ssse3.c",
+    "flac/src/libFLAC/stream_encoder_intrin_avx2.c",
+    "flac/src/libFLAC/stream_encoder_framing.c",
+    "flac/src/libFLAC/window.c",
 };
--- src/lib.zig
+++ src/lib.zig
@@ -3,130 +3,202 @@ const c = @cImport(@cInclude("FLAC/stream_decoder.h"));
 
 const Flac = @This();
 
-num_channels: u8,
+channels: u8,
+sample_rate: u24,
+samples: []i32,
 
 pub const DecodeError = error{
     OutOfMemory,
+    InvalidData,
 };
 
-pub fn decodeStream(reader: anytype) Flac {
-    const Dec = Decoder(@TypeOf(reader));
+pub fn decodeStream(allocator: std.mem.Allocator, stream: std.io.StreamSource) (DecodeError || std.io.StreamSource.ReadError)!Flac {
+    var data = DecodeData{ .allocator = allocator, .stream = stream };
     var decoder = c.FLAC__stream_decoder_new() orelse return error.OutOfMemory;
-    c.FLAC__stream_decoder_init_stream(decoder, Dec.readCallback, null, null, null, null, Dec.writeCallback, reader);
-}
 
-fn Decoder(comptime Reader: type) type {
-    return struct {
-        fn readCallback(_: [*c]const c.FLAC__StreamDecoder, buffer: [*c]c.FLAC__byte, bytes: [*c]usize, user_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderReadStatus {
-            const flac = @ptrCast(*Reader, user_data);
+    switch (c.FLAC__stream_decoder_init_stream(
+        decoder,
+        readCallback,
+        seekCallback,
+        tellCallback,
+        lengthCallback,
+        eofCallback,
+        writeCallback,
+        metadataCallback,
+        errorCallback,
+        &data,
+    )) {
+        c.FLAC__STREAM_DECODER_INIT_STATUS_OK => {},
+        c.FLAC__STREAM_DECODER_INIT_STATUS_UNSUPPORTED_CONTAINER => unreachable,
+        c.FLAC__STREAM_DECODER_INIT_STATUS_INVALID_CALLBACKS => unreachable,
+        c.FLAC__STREAM_DECODER_INIT_STATUS_MEMORY_ALLOCATION_ERROR => return error.OutOfMemory,
+        c.FLAC__STREAM_DECODER_INIT_STATUS_ERROR_OPENING_FILE => unreachable,
+        c.FLAC__STREAM_DECODER_INIT_STATUS_ALREADY_INITIALIZED => unreachable,
+        else => unreachable,
+    }
 
-            if (bytes != null) {
-                bytes.* = flac.reader.read(buffer[0..bytes.*]) catch return c.FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
-
-                if (bytes.* == 0) {
-                    return c.FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
-                }
-
-                return c.FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
-            }
-
-            return c.FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+    if (c.FLAC__stream_decoder_process_until_end_of_stream(decoder) == 0) {
+        switch (data.status) {
+            c.FLAC__STREAM_DECODER_ERROR_STATUS_LOST_SYNC => unreachable,
+            c.FLAC__STREAM_DECODER_ERROR_STATUS_BAD_HEADER => return error.InvalidData,
+            c.FLAC__STREAM_DECODER_ERROR_STATUS_FRAME_CRC_MISMATCH => return error.InvalidData,
+            c.FLAC__STREAM_DECODER_ERROR_STATUS_UNPARSEABLE_STREAM => return error.InvalidData,
+            c.FLAC__STREAM_DECODER_ERROR_STATUS_BAD_METADATA => return error.InvalidData,
+            else => unreachable,
         }
+    }
 
-        fn writeCallbackfn(_: [*c]const c.FLAC__StreamDecoder, frame: [*c]const c.FLAC__Frame, buffer: [*c]const [*c]const c.FLAC__int32, user_data: ?*anyopaque) callconv(.C) c.FLAC__StreamDecoderWriteStatus {
-            _ = frame;
-            _ = buffer;
-            _ = user_data;
-            return c.FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
-
-            //             Sint16 *data;
-            // unsigned int i, j, channels;
-            // int shift_amount = 0;
-
-            // if (!music->stream) {
-            //     return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
-            // }
-
-            // switch (music->bits_per_sample) {
-            // case 16:
-            //     shift_amount = 0;
-            //     break;
-            // case 20:
-            //     shift_amount = 4;
-            //     break;
-            // case 24:
-            //     shift_amount = 8;
-            //     break;
-            // default:
-            //     SDL_SetError("FLAC decoder doesn't support %d bits_per_sample", music->bits_per_sample);
-            //     return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
-            // }
-
-            // if (music->channels == 3) {
-            //     /* We'll just drop the center channel for now */
-            //     channels = 2;
-            // } else {
-            //     channels = music->channels;
-            // }
-
-            // data = SDL_stack_alloc(Sint16, (frame->header.blocksize * channels));
-            // if (!data) {
-            //     SDL_SetError("Couldn't allocate %d bytes stack memory", (int)(frame->header.blocksize * channels * sizeof(*data)));
-            //     return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
-            // }
-            // if (music->channels == 3) {
-            //     Sint16 *dst = data;
-            //     for (i = 0; i < frame->header.blocksize; ++i) {
-            //         Sint16 FL = (buffer[0][i] >> shift_amount);
-            //         Sint16 FR = (buffer[1][i] >> shift_amount);
-            //         Sint16 FCmix = (Sint16)((buffer[2][i] >> shift_amount) * 0.5f);
-            //         int sample;
-
-            //         sample = (FL + FCmix);
-            //         if (sample > SDL_MAX_SINT16) {
-            //             *dst = SDL_MAX_SINT16;
-            //         } else if (sample < SDL_MIN_SINT16) {
-            //             *dst = SDL_MIN_SINT16;
-            //         } else {
-            //             *dst = sample;
-            //         }
-            //         ++dst;
-
-            //         sample = (FR + FCmix);
-            //         if (sample > SDL_MAX_SINT16) {
-            //             *dst = SDL_MAX_SINT16;
-            //         } else if (sample < SDL_MIN_SINT16) {
-            //             *dst = SDL_MIN_SINT16;
-            //         } else {
-            //             *dst = sample;
-            //         }
-            //         ++dst;
-            //     }
-            // } else {
-            //     for (i = 0; i < channels; ++i) {
-            //         Sint16 *dst = data + i;
-            //         for (j = 0; j < frame->header.blocksize; ++j) {
-            //             *dst = (buffer[i][j] >> shift_amount);
-            //             dst += channels;
-            //         }
-            //     }
-            // }
-            // SDL_AudioStreamPut(music->stream, data, (frame->header.blocksize * channels * sizeof(*data)));
-            // SDL_stack_free(data);
-
-            // return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
-            //     }
-        }
-
-        fn metadataCallbackfn(_: [*c]const c.FLAC__StreamDecoder, metadata: [*c]const c.FLAC__StreamMetadata, user_data: ?*anyopaque) callconv(.C) void {
-            _ = metadata;
-            _ = user_data;
-        }
+    return .{
+        .channels = data.channels,
+        .sample_rate = @intCast(data.sample_rate),
+        .samples = data.samples,
     };
 }
 
+const DecodeData = struct {
+    allocator: std.mem.Allocator,
+    stream: std.io.StreamSource,
+    channels: u8 = 0,
+    sample_rate: u24 = 0,
+    bits_per_sample: u8 = 0,
+    bytes_per_sample: u8 = 0,
+    total_samples: usize = 0,
+    status: c.FLAC__StreamDecoderErrorStatus = undefined,
+    samples: []i32 = &.{},
+    sample_index: usize = 0,
+};
+
+fn readCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    buffer: [*c]c.FLAC__byte,
+    bytes: [*c]usize,
+    user_data: ?*anyopaque,
+) callconv(.C) c.FLAC__StreamDecoderReadStatus {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+
+    if (bytes.* > 0) {
+        bytes.* = data.stream.read(buffer[0..bytes.*]) catch return c.FLAC__STREAM_DECODER_READ_STATUS_END_OF_STREAM;
+        return c.FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
+    }
+
+    return c.FLAC__STREAM_DECODER_READ_STATUS_ABORT;
+}
+
+fn seekCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    absolute_byte_offset: u64,
+    user_data: ?*anyopaque,
+) callconv(.C) c.FLAC__StreamDecoderSeekStatus {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    data.stream.seekTo(absolute_byte_offset) catch return c.FLAC__STREAM_DECODER_SEEK_STATUS_ERROR;
+    return c.FLAC__STREAM_DECODER_SEEK_STATUS_OK;
+}
+
+fn tellCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    absolute_byte_offset: [*c]u64,
+    user_data: ?*anyopaque,
+) callconv(.C) c.FLAC__StreamDecoderTellStatus {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    absolute_byte_offset.* = data.stream.getPos() catch return c.FLAC__STREAM_DECODER_TELL_STATUS_ERROR;
+    return c.FLAC__STREAM_DECODER_TELL_STATUS_OK;
+}
+
+fn lengthCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    stream_length: [*c]u64,
+    user_data: ?*anyopaque,
+) callconv(.C) c.FLAC__StreamDecoderLengthStatus {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    stream_length.* = data.stream.getEndPos() catch return c.FLAC__STREAM_DECODER_LENGTH_STATUS_ERROR;
+    return c.FLAC__STREAM_DECODER_LENGTH_STATUS_OK;
+}
+
+fn eofCallback(_: [*c]const c.FLAC__StreamDecoder, user_data: ?*anyopaque) callconv(.C) c_int {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    const pos = data.stream.getPos() catch return 1;
+    const end_pos = data.stream.getEndPos() catch return 1;
+    return @intFromBool(pos == end_pos);
+}
+
+fn writeCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    frame: [*c]const c.FLAC__Frame,
+    buffer: [*c]const [*c]const c.FLAC__int32,
+    user_data: ?*anyopaque,
+) callconv(.C) c.FLAC__StreamDecoderWriteStatus {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+
+    if (data.total_samples == 0) {
+        if (frame.*.header.blocksize > data.samples.len - data.sample_index) {
+            const size = data.samples.len + data.channels * frame.*.header.blocksize * data.bytes_per_sample;
+            data.samples = data.allocator.realloc(data.samples, size) catch {
+                return c.FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+            };
+        }
+    } else {
+        if (data.samples.len == 0) {
+            const size = data.total_samples * data.bytes_per_sample;
+            data.samples = data.allocator.alloc(i32, size) catch {
+                return c.FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+            };
+        }
+    }
+
+    const shift_amount: u5 = @intCast(32 - data.bits_per_sample);
+    if (frame.*.header.channels == 3) {
+        for (0..frame.*.header.blocksize) |i| {
+            const center = @divExact(buffer[2][i] << shift_amount, 2);
+            const left = (buffer[0][i] << shift_amount) + center;
+            const right = (buffer[1][i] << shift_amount) + center;
+            data.samples[data.sample_index] = left;
+            data.samples[data.sample_index + 1] = right;
+            data.sample_index += 2;
+        }
+    } else {
+        for (0..frame.*.header.blocksize) |i| {
+            for (0..data.channels) |ch| {
+                const sample = buffer[ch][i];
+                data.samples[data.sample_index] = sample << shift_amount;
+                data.sample_index += 1;
+            }
+        }
+    }
+
+    return c.FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
+}
+
+fn metadataCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    metadata: [*c]const c.FLAC__StreamMetadata,
+    user_data: ?*anyopaque,
+) callconv(.C) void {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    data.channels = switch (metadata.*.data.stream_info.channels) {
+        3 => 2, // We'll drop the center channel and mix it with Left and Right channels
+        else => @intCast(metadata.*.data.stream_info.channels),
+    };
+    data.sample_rate = @intCast(metadata.*.data.stream_info.sample_rate);
+    data.bits_per_sample = @intCast(metadata.*.data.stream_info.bits_per_sample);
+    data.bytes_per_sample = data.bits_per_sample / 8;
+    data.total_samples = metadata.*.data.stream_info.total_samples;
+}
+
+fn errorCallback(
+    _: [*c]const c.FLAC__StreamDecoder,
+    status: c.FLAC__StreamDecoderErrorStatus,
+    user_data: ?*anyopaque,
+) callconv(.C) void {
+    const data = @as(*DecodeData, @ptrCast(@alignCast(user_data)));
+    data.status = status;
+}
+
 test {
-    const test_file = @embedFile("");
-    var fbs = std.io.fixedBufferStream(test_file);
-    decodeStream(fbs.reader());
+    const test_file = @embedFile("center.flac");
+    const fbs = std.io.fixedBufferStream(test_file);
+    var stream = std.io.StreamSource{ .const_buffer = fbs };
+    const out = try decodeStream(std.testing.allocator, stream);
+    try std.testing.expect(out.samples.len > 0);
+    try std.fs.cwd().writeFile("zig-out/raw.pcm", std.mem.sliceAsBytes(out.samples));
+    std.testing.allocator.free(out.samples);
 }
