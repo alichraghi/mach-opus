d9dfb5f44cd3d80f880520b33b0eb502604c93f0
Ali Chraghi <alichraghi@proton.me>
2023-07-26 13:36:26 +0330

    update

--- build.zig
+++ build.zig
@@ -2,20 +2,20 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("mach-flac", .{ .source_file = .{ .path = "src/lib.zig" } });
+    _ = b.addModule("mach-opus", .{ .source_file = .{ .path = "src/lib.zig" } });
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const libflac_dep = b.dependency("libFLAC", .{ .target = target, .optimize = optimize });
+    const libopusfile_dep = b.dependency("opusfile", .{ .target = target, .optimize = optimize });
 
     const main_test = b.addTest(.{
         .root_source_file = .{ .path = "src/lib.zig" },
         .target = target,
         .optimize = optimize,
     });
-    main_test.linkLibrary(libflac_dep.artifact("flac"));
-    b.installArtifact(main_test);
+    main_test.linkLibrary(libopusfile_dep.artifact("opusfile"));
+    main_test.main_pkg_path = ".";
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&b.addRunArtifact(main_test).step);
